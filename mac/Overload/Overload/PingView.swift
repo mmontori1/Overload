19a7ac901e9feaedcedf5be0c5f22535339272f4
Mariano <mmontori@umich.edu>
2017-06-19 23:08:57 -0400

    mac start/stop pinging in console
    -using PlainPing CocoaPod to handle pinging
    -now has a Start/Stop button
    -prints ping latency in console

---
+++ mac/Overload/Overload/PingView.swift
@@ -0,0 +1,17 @@
+//
+//  PingView.swift
+//  Overload
+//
+//  Created by Mariano Montori on 6/19/17.
+//  Copyright © 2017 Mariano Montori. All rights reserved.
+//
+
+import Cocoa
+
+class PingView: NSView {
+    
+    @IBOutlet weak var imageView: NSImageView!
+    @IBOutlet weak var PingText: NSTextField!
+    @IBOutlet weak var PingValue: NSTextField!
+
+}
--- mac/Overload/Overload/StatusMenuController.swift
+++ mac/Overload/Overload/StatusMenuController.swift
@@ -6,12 +6,32 @@
 //  Copyright © 2017 Mariano Montori. All rights reserved.
 //
 
+import Foundation
 import Cocoa
+import PlainPing
 
 class StatusMenuController: NSObject {
     @IBOutlet weak var statusMenu: NSMenu!
-    
+
     let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    
+    var timer = Timer()
+    var isPinging = false
+    
+    func startPing() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(self.ping)), userInfo: nil, repeats: true)
+    }
+    
+    func ping() {
+        PlainPing.ping("104.160.131.1", withTimeout: 1.0, completionBlock: { (timeElapsed:Double?, error:Error?) in
+            if let latency = timeElapsed {
+                print("latency (ms): \(latency)")
+            }
+            if let error = error {
+                print("error: \(error.localizedDescription)")
+            }
+        })
+    }
     
     override func awakeFromNib() {
         let icon = NSImage(named: "statusIcon")
@@ -19,6 +39,17 @@ class StatusMenuController: NSObject {
         // best for dark mode
         statusItem.image = icon
         statusItem.menu = statusMenu
+    }
+    
+    @IBAction func startClicked(_ sender: NSMenuItem) {
+        if(!isPinging){
+            startPing()
+            isPinging = true;
+        }
+        else{
+            timer.invalidate()
+            isPinging = false;
+        }
     }
     
     @IBAction func quitClicked(sender: NSMenuItem) {
